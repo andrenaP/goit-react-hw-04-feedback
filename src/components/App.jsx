a1351fa93b01fdc43d0dd97c7f7feadee3e097bd
andrenaP <a0993100106@gmail.com>
2023-08-08 11:35:58 +0300

    fix one warning

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Feedback from './FeedbackOptions';
 import Statistics from './Statistics';
 import Section from './Section';
